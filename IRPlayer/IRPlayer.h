96b3cd62b2bf7357559f6fc3637ee8dd11076fd4
Phil.Chang <wen-chuan.chang@senao.com>
2019-07-05 15:28:04 +0800

    Initial Commit

---
+++ IRPlayer/IRPlayer.h
@@ -0,0 +1,19 @@
+//
+//  IRPlayer.h
+//  IRPlayer
+//
+//  Created by Phil on 2019/7/5.
+//  Copyright © 2019 Phil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IRPlayer.
+FOUNDATION_EXPORT double IRPlayerVersionNumber;
+
+//! Project version string for IRPlayer.
+FOUNDATION_EXPORT const unsigned char IRPlayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IRPlayer/PublicHeader.h>
+
+
