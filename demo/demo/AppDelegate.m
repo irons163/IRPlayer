fbe52bb4e1c7d84a46d46be3b028ef5033e94eaa
Your Name <you@example.com>
2020-08-05 15:49:15 +0800

    demo update

--- demo/demo/AppDelegate.m
+++ demo/demo/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import <AVFoundation/AVFoundation.h>
 
 @interface AppDelegate ()
 
@@ -16,7 +17,9 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    
+    [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayback error:nil];
+    
     return YES;
 }
 
